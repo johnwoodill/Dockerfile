78d6fd4c67757a0c6f0ae59ac3c5f0fb247d9571
johnwoodill <johnwoodill@gmail.com>
2018-03-18 10:19:09 -1000

    uypdating packages

---
+++ r-packages.R
@@ -0,0 +1,11 @@
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "http://cran.cnr.berkeley.edu/"
+  options(repos = r)
+})
+
+install.packages(c("tidyverse",
+                   "stringr",
+                   "zoo", 
+                   "lfe"))
+
