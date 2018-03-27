2b3ef512165bebf06d9201ac47e22ce6de93cbb0
johnwoodill <johnwoodill@gmail.com>
2018-03-26 18:07:39 -1000

    fixing permissions

--- ami-installer.sh
+++ ami-installer.sh
@@ -1,33 +1,36 @@
 #!/bin/bash -x
 
-#sudo -s
-
-#adduser john
-# Set password for john `passwd john`
 usermod -aG wheel johnwoodill
 
-# switch to user john for R
+# Install necessary packages
+yum -y update 
+yum -y install epel-release wget htop libcurl libcurl-devel libxml2 libxml2-devel curl openssl openssl-devel postgresql postgresql-devel libpng libpng-devel libjpeg libjpeg-devel proj proj-devel proj-epsg proj-nad udunits2 udunits2-devel gdal gdal-devel gdal-libs R-java R-cpp libcurl-devel git
+
+yum -y groupinstall "Development Tools"
+
+yum -y install R-core R-devel
+
+# Install rstudio server
+echo("Installing Rstudio Server")
+wget https://download2.rstudio.org/rstudio-server-rhel-1.1.442-x86_64.rpm
+yum -y install rstudio-server-rhel-1.1.442-x86_64.rpm
+sudo systemctl status rstudio-server.service
+sudo systemctl enable rstudio-server.service
+
+echo("Installing r-packages.R")
+Rscript r-packages.R
+
+sudo johnwoodill
+
+# Install Dropbox-Uploader
+curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
+chmod +x dropbox_uploader.sh
+./dropbox_uploader.sh
 
 # Install Anaconda
 #wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 #bash Anaconda3-4.3.0-Linux-x86_64.sh
 
-
-# Install necessary packages
-yum update -y
-yum -y install epel-release wget htop libcurl libcurl-devel libxml2 libxml2-devel curl openssl openssl-devel postgresql postgresql-devel libpng libpng-devel libjpeg libjpeg-devel proj proj-devel proj-epsg proj-nad udunits2 udunits2-devel gdal gdal-devel gdal-libs R-java R-cpp libcurl-devel git
-
-yum groupinstall "Development Tools"
-
-yum -y install R-core R-devel
-
-# Install rstudio server
-wget https://download2.rstudio.org/rstudio-server-rhel-1.1.442-x86_64.rpm
-yum install rstudio-server-rhel-1.1.442-x86_64.rpm
-sudo systemctl status rstudio-server.service
-sudo systemctl enable rstudio-server.service
-
-Rscript r-packages.R
 
 # Dropbox: https://github.com/andreafabrizi/Dropbox-Uploader
 
